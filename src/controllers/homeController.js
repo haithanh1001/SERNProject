0534cab78e95654d80737053acb09d8f3106ecd8
haithanh <haithanh9718@gmail.com>
2022-11-21 14:56:31 +0700

    finished ORM CRUD

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,5 @@
 let db = require("../models/index");
-
+import CRUDService from "../services/CRUDService";
 let getHomePage = async (req, res) => {
   try {
     let data = await db.User.findAll();
@@ -15,7 +15,59 @@ let getAboutPage = (req, res) => {
   return res.render("test/about");
 };
 
+let getCRUD = async (req, res) => {
+  return res.render("crud");
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  return res.send(message);
+};
+
+let displayGetCRUD = async (req, res) => {
+  let data = await CRUDService.getAllUser();
+  console.log(data);
+  return res.render("displayCRUD", {
+    dataTable: data,
+  });
+};
+
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  console.log(userId);
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId);
+
+    return res.render("editCRUD", {
+      user: userData,
+    });
+  } else {
+    return res.send("User not found");
+  }
+};
+
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  await CRUDService.updateUserData(data);
+  return res.redirect("/get-crud");
+};
+
+let deleteCRUD = async (req, res) => {
+  let id = req.query.id;
+  if (id) {
+    await CRUDService.deleteUserById(id);
+    return res.redirect("/get-crud");
+  } else {
+    res.send("user not found");
+  }
+};
 module.exports = {
   getHomePage,
   getAboutPage,
+  getCRUD,
+  postCRUD,
+  displayGetCRUD,
+  getEditCRUD,
+  putCRUD,
+  deleteCRUD,
 };
---
+++ src/services/CRUDService.js
@@ -0,0 +1,103 @@
+import db from "../models/index";
+import bcrypt from "bcryptjs";
+const salt = bcrypt.genSaltSync(10);
+
+let createNewUser = async (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+      await db.User.create({
+        ...data,
+        password: hashPasswordFromBcrypt,
+        gender: data.gender === "1" ? true : false,
+      });
+      resolve("Ok create new user succeed");
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+let hashUserPassword = (password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      var hashPassword = await bcrypt.hashSync(password, salt);
+      resolve(hashPassword);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+let getAllUser = () => {
+  return new Promise((resolve, reject) => {
+    try {
+      let users = db.User.findAll({
+        raw: true,
+      });
+      resolve(users);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+let getUserInfoById = (userId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({ where: { id: userId }, raw: true });
+      if (user) {
+        resolve(user);
+      } else {
+        resolve([]);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+let updateUserData = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: data.id },
+      });
+      if (user) {
+        user.firstName = data.firstName;
+        user.lastName = data.lastName;
+        user.address = data.address;
+        await user.save();
+        resolve();
+      } else {
+        resolve();
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+let deleteUserById = (id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: id },
+      });
+      if (user) {
+        await user.destroy();
+        resolve();
+      } else {
+        resolve();
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+module.exports = {
+  createNewUser,
+  getAllUser,
+  getUserInfoById,
+  updateUserData,
+  deleteUserById,
+};
